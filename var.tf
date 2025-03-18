d49c887a13deb05989c6395ef234128ecdc46b92
ajay <ajay@gmail.com>
2025-03-18 13:10:52 +0530

    update

---
+++ var.tf
@@ -0,0 +1,26 @@
+variable "AWS_REGION" {
+    default = "ap-southeast-1"
+}
+variable "AWS_AMI" {
+    default = "ami-0b03299ddb99998e9"
+}
+variable "AWS_AMI_UBUNTU" {
+    default = "ami-060e277c0d4cce553"
+}
+variable "az" {
+    default = "ap-southeast-1a"
+}
+variable "akey" {
+    default = "AK"
+}
+variable "skey" {
+    default = "PK"
+}
+variable "pkey" {
+    default = "Linux-Singapoore-2025"
+}
+variable "rpass" {
+    default = "111"
+}
+
+
